30c44d8cdbdbc289a89836192c45d6faa2694411
joannakaczmarczyk <joanna@98elements.com>
2020-04-16 15:29:19 +0200

    Spring Boot 2 + jOOQ examples (postgres))

---
+++ db/init.sql
@@ -0,0 +1,1 @@
+create table books(id int not null primary key, title varchar(255) not null);
--- src/main/resources/db/migration/V1__schema_init.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE books (
-  id             INT          NOT NULL PRIMARY KEY,
-  title          VARCHAR(100) NOT NULL
-);
- 
